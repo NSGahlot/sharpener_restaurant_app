a416429286ef26240623a37c7fac0ec1a870ee51
NSGahlot <ngahlot411@gmail.com>
2024-09-06 11:30:29 +0530

    refactor props value

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -3,7 +3,7 @@ import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
-const HeaderCartButton = (props) => {
+const HeaderCartButton = ({ onClick }) => {
   const cartCtx = useContext(CartContext);
 
   const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
@@ -11,7 +11,7 @@ const HeaderCartButton = (props) => {
   }, 0);
 
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={classes.button} onClick={onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
